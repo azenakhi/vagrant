df580fe418f3ac1183b3522ab8348c92f0679c74
Ahmed ZENAKHI <ahmed.zenakhi@gmail.com>
2015-03-08 16:54:10 +0100

    Added script for updating OS and docker build image from GitHub

--- install.sh
+++ install.sh
@@ -1,2 +1,8 @@
 #!/usr/bin/env bash  
 # Install Ansible inside the Vagrant Ubuntu VM
+
+yum -y update
+yum -y install docker-io
+service docker start
+docker build -t nginx https://github.com/azenakhi/docker.git
+
