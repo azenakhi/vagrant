6467918121161a2dcf657db95a09e6e86082e359
azenakhi <ahmed.zenakhi@gmail.com>
2016-12-18 14:21:29 +0100

    Delete install.sh

--- install.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash  
-# Install Ansible inside the Vagrant Ubuntu VM
-
-yum -y update
-yum -y install docker-io
-service docker start
-docker build -t nginx https://github.com/azenakhi/docker.git
-
